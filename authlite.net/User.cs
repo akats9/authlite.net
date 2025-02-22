fd667d3205f18a1e2bda2618cc3f834ef615b81c
akats9 <akatsigiann@gmail.com>
2025-02-22 18:06:28 +0200

    Added db connected get;set; for password with hashing

--- authlite.net/User.cs
+++ authlite.net/User.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Data.Sqlite;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace authlite.net;
 
@@ -6,9 +8,8 @@ public class User {
     public string Email {
         get {
             using (var connection = new SqliteConnection("Data Source=Data/auth.db")) {
-                var command = connection.CreateCommand();
-                
                 connection.Open();
+                var command = connection.CreateCommand();
                 using (var reader = command.ExecuteReader()) {
                     return reader.GetString(0);
                 }
@@ -27,9 +28,8 @@ public class User {
     public string Username { 
         get {
             using (var connection = new SqliteConnection("Data Source=Data/auth.db")) {
-                var command = connection.CreateCommand();
-                
                 connection.Open();
+                var command = connection.CreateCommand();
                 using (var reader = command.ExecuteReader()) {
                     return reader.GetString(1);
                 }
@@ -40,12 +40,39 @@ public class User {
                 connection.Open();
 
                 var command = connection.CreateCommand();
-                command.CommandText = @"INSERT INTO Users (Username) VALUES ('username');";
-                command.Parameters.AddWithValue("username", value);
+                command.CommandText = @"INSERT INTO Users (Username) VALUES ('$username');";
+                command.Parameters.AddWithValue("$username", value);
             }  
         } 
     }
-    public string Password { get; set; }
+
+    public string Password {
+        get {
+            using (var connection = new SqliteConnection("Data Source=Data/auth.db")) {
+                connection.Open();
+                var command = connection.CreateCommand();
+                using (var reader = command.ExecuteReader()) {
+                    return reader.GetString(2);
+                }
+            }
+        }
+        set {
+            using (var connection = new SqliteConnection("Data Source=Data/auth.db")) {
+                connection.Open();
+                var command = connection.CreateCommand();
+
+                using (SHA256 hasher = SHA256.Create()) {
+                    byte[] valueToBytes = Encoding.ASCII.GetBytes(value);
+                    byte[] passwordHashValue = hasher.ComputeHash(valueToBytes);
+                    
+                    var hashString = Encoding.ASCII.GetString(passwordHashValue);
+
+                    command.CommandText = @"INSERT INTO Users (Password) VALUES ('$password');";
+                    command.Parameters.AddWithValue("$password", hashString);
+                }
+            }
+        }
+    }
     public string Id { get; set; }
     public bool Confirmed { get; set; }
     public role Role { get; set; }
